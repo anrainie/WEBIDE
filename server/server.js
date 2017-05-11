b8745348a9794722d2f5c1830258a2054e162225
zcn <912555919@qq.com>
2017-05-11 16:31:07 +0800

    增加session，移动express代码到server.js

--- server/server.js
+++ server/server.js
@@ -1,45 +1,17 @@
-/*
- *  载入依赖项
- *  fs：文件系统； path：href解决文案；
- *  body-parser：解析Request body和处理req
- *  api：处理前台请求的接口
- */
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
-const validator = require('express-validator');
-const app = express();
-const resolve = file => path.resolve(__dirname, file);
+var express = require('express');
+var webpack = require('webpack');
+var session = require('express-session');
 
-const cookieParser = require('cookie-parser')
-const session = require('express-session');
-// const User = require('./db').User;
-//const flash=require('connect-flash');
-//const messages=require('express-messages');
+var app = express();
 
-/*
- * 监听3300端口
- * 用JSON格式处理bodyParser请求
- */
-app.set('port', (process.env.port || 3300));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+//session
+var sessionStore = new session.MemoryStore({reapInterval: 60000 * 10});
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret:'agree',
+    key:'ide',
+    store: sessionStore
+}));
 
-
-/**
- * 设置静态资源目录为dist
- * 排除api接口的路由，向浏览器发送根文件
- */
-app.use('/dist', express.static(resolve('../dist')));
-app.get('/', function (req, res, next) {
-    const html = fs.readFileSync(resolve('../index.html'), 'utf-8');
-    res.send(html);
-});
-
-app.listen(app.get('port'), function () {
-    console.log('Server up: http://localhost:' + app.get('port'));
-});
-
-
-
+module.exports  = app;
