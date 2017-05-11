3f3c74baf9f9ed9e4496c8e5942a075668818605
zcn <912555919@qq.com>
2017-05-11 17:37:38 +0800

    修改image loader。修改center.vue样式

--- src/views/center/center.vue
+++ src/views/center/center.vue
@@ -1,24 +1,44 @@
 <template>
-    <div>
-        <input type="text" v-model="message"/>
-
-        <router-link :to="'afa'">{{message}}</router-link>
-        <router-link :to="'aweb'">AWEB</router-link>
-
+    <div class="entrys">
+        <router-link :to="'afa'" class="entry">
+            <p>AFA</p>
+        </router-link>
+        <router-link :to="'aweb'" class = "entry">
+            <p>AWEB</p>
+        </router-link>
     </div>
 </template>
-
+<style>
+    .entrys{
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        background-image: url("~assets/image/center-bg.png");
+    }
+    .entry{
+        position: relative;
+        display: inline-block;
+        width: 200px;
+        height: 200px;
+        text-align: center;
+        top:20%;
+        background-color: gray;
+        margin-left: 20px;
+        border-radius:10px;
+    }
+    .entry p{
+        position: relative;
+        top: 35%;
+        color: black;
+        text-decoration: none;
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    }
+</style>
 <script>
     export default{
         data(){
-            return {
-                message: 'Center'
-            }
         },
         methods: {
-            reverseMessage: function () {
-                this.message = this.message.split('').reverse().join('')
-            }
         }
     }
 </script>
