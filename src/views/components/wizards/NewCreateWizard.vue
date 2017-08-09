9ee2e8f5f9e362f6d8961e3ac7b90333bc9e95d7
liling <1045479083@qq.com>
2017-08-09 10:04:16 +0800

    *增加向导会话框点击确定触发创建资源事件

--- src/views/components/wizards/NewCreateWizard.vue
+++ src/views/components/wizards/NewCreateWizard.vue
@@ -15,34 +15,52 @@
         </el-form>
         <div slot="footer" class="dialog-footer">
             <el-button @click="dialogFormVisible = false">取 消</el-button>
-            <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+            <el-button type="primary" @click="handleOk">确 定</el-button>
         </div>
     </el-dialog>
 </template>
 <script>
-    export default {
-        data() {
-            return {
-              dialogFormVisible : true,
-              labelWidth: '140px',
-              name:'',
-              desc:'',
-              wizardtitle: 'wizardtitle1',
-              pagetitle: 'pagetitile1',
-              pagedesc: 'pagedesc1',
-              namelabel: 'namelabel1',
-              desclabel: 'desclabel1'
+  export default {
+    data() {
+      return {
+        dialogFormVisible : true,
+        labelWidth: '140px',
+        name:'',
+        desc:'',
+        resourceId:'',
+        path:'',
+        wizardtitle: '',
+        pagetitle: '',
+        pagedesc: '',
+        namelabel: '',
+        desclabel: ''
+      }
+    },
+    component: {},
+    methods:{
+      handleClose(done){
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
+      },
+      handleOk(){
+        this.dialogFormVisible = false;
+        IDE.socket.emit("createNewResource",{
+          type: IDE.type,
+          event: 'createNewResource',
+          data: {path: this.path,resourceId:this.resourceId,name:this.name,desc:this.desc}
+        }, function (data) {
+          if (data) {
+            let result = JSON.parse(data);
+            if (result.state === 'success') {
+              let path = result.data.path;
+              IDE.navigator.refresh(path);
             }
-        },
-        component: {},
-        methods:{
-          handleClose(done){
-            this.$confirm('确认关闭？')
-              .then(_ => {
-                done();
-              })
-              .catch(_ => {});
-          },
-        }
+          }
+        });
+      }
     }
+  }
 </script>
