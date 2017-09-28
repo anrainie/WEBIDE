8b6caa1326a992e41588c6fb3f4fc60bedb3295c
anrainie <anrainie@163.com>
2017-09-28 13:40:40 +0800

    babel jsx支持

--- src/views/afe/editor/msgConfigEditor.vue
+++ src/views/afe/editor/msgConfigEditor.vue
@@ -264,7 +264,6 @@
                 </div>
             </div>
         </div>
-
     </editorContainer>
 
 
--- src/views/afe/editor/routerConfigEditor.vue
+++ src/views/afe/editor/routerConfigEditor.vue
@@ -1,13 +1,43 @@
 <template>
-    <!--报文格式编辑器-->
+    <!--路由编辑器-->
     <editorContainer :editor="this">
-        <el-row>
-            <el-col :span="16">
-                <MyTable :model="model.source" :tableConfig="METHOD_TABLE_CONFIG"></MyTable>
-
-            </el-col>
-        </el-row>
-
+        <div slot="editor-content" style=" border: 1px solid;    height: 100%;">
+            <el-row>
+                <el-col :span="12">
+                    <el-table
+                            :data="input.Routing.ConditionGroups.Group"
+                            style="width: 100%">
+                        <el-table-column type="expand">
+                            <template scope="props">
+                                <el-row>
+                                    <el-col :span="10">
+                                        <span>源路由:{{props.row.Set}}</span>
+                                    </el-col>
+                                </el-row>
+                                <el-row>
+                                    <el-col :span="24">
+                                        <el-tree
+                                                :data="props.row.Set"
+                                                node-key="-Name"
+                                                default-expand-all>
+                                        </el-tree>
+                                    </el-col>
+                                </el-row>
+                            </template>
+                        </el-table-column>
+                        <el-table-column label="分组ID">
+                            <template scope="props">
+                                <label>
+                                    <input type="text" v-model="props.row['-Name']"
+                                           style="width:80px;height:100%;border:none;background-color:inherit;overflow: hidden;text-overflow:ellipsis;white-space: nowrap;"/>
+                                </label>
+                            </template>
+                        </el-table-column>
+                        <el-table-column label="数量" prop="Set" :formatter="getSize"></el-table-column>
+                    </el-table>
+                </el-col>
+            </el-row>
+        </div>
     </editorContainer>
 </template>
 <style>
@@ -39,6 +69,24 @@
                     name: ""
                 },
             };
+        },
+        mounted(){
+            console.log(this.input);
+        },
+
+        methods: {
+            getSize(r, c){
+                let set = r.Set;
+                if (set == null)return 0;
+                if (set instanceof Array)return set.length;
+                else return 1;
+            },
+            isDirty(){
+            },
+            save(){
+            },
+            focus(){
+            },
         },
         components: {
             editorContainer: editorContainer,
