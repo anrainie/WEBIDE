84a86fbf98fe4b54713d18adc2ba3b036392af82
anrainie <anrainie@163.com>
2017-09-28 17:28:11 +0800

    babel jsx支持

--- src/views/afe/editor/routerConfigEditor.vue
+++ src/views/afe/editor/routerConfigEditor.vue
@@ -5,35 +5,44 @@
             <el-row>
                 <el-col :span="12">
                     <el-table
-                            :data="input.Routing.ConditionGroups.Group"
+                            :data="input.Conditions"
                             style="width: 100%">
                         <el-table-column type="expand">
                             <template scope="props">
                                 <el-row>
-                                    <el-col :span="10">
-                                        <span>源路由:{{props.row.Set}}</span>
-                                    </el-col>
-                                </el-row>
-                                <el-row>
                                     <el-col :span="24">
                                         <el-tree
                                                 :data="props.row.Set"
-                                                node-key="-Name"
-                                                default-expand-all>
+                                                :props="itemProp"
+                                                node-key="Name"
+                                                :expand-on-click-node="false"
+                                                :render-content="renderSourceRouter">
                                         </el-tree>
                                     </el-col>
                                 </el-row>
                             </template>
                         </el-table-column>
-                        <el-table-column label="分组ID">
+                        <el-table-column label="源路由分组">
                             <template scope="props">
                                 <label>
-                                    <input type="text" v-model="props.row['-Name']"
+                                    <input type="text" v-model="props.row['Name']"
                                            style="width:80px;height:100%;border:none;background-color:inherit;overflow: hidden;text-overflow:ellipsis;white-space: nowrap;"/>
                                 </label>
                             </template>
                         </el-table-column>
-                        <el-table-column label="数量" prop="Set" :formatter="getSize"></el-table-column>
+                        <el-table-column label="总数" prop="Set" :formatter="getSize"></el-table-column>
+                        <el-table-column label="操作">
+                            <template scope="scope">
+                                <el-button @click="addRouter(scope)" type="text"
+                                           size="small">
+                                    添加路由
+                                </el-button>
+                                <el-button @click="removeGroup(scope)" type="text"
+                                           size="small">
+                                    移除组
+                                </el-button>
+                            </template>
+                        </el-table-column>
                     </el-table>
                 </el-col>
             </el-row>
@@ -56,6 +65,9 @@
             return {
                 sourceDialogVisible: false,
                 targetDialogVisible: false,
+                itemProp: {
+                    children: 'Item',
+                },
                 dirty: false,
                 selectSource: {},
                 selectTarget: {},
@@ -71,15 +83,17 @@
             };
         },
         mounted(){
-            console.log(this.input);
+            window.input = this.input;
+            console.log('input', this.input);
         },
 
         methods: {
             getSize(r, c){
-                let set = r.Set;
-                if (set == null)return 0;
-                if (set instanceof Array)return set.length;
-                else return 1;
+//                let set = r.Set;
+//                if (set == null)return 0;
+//                if (set instanceof Array)return set.length;
+//                else return 1;
+                return r.Set.length;
             },
             isDirty(){
             },
@@ -87,6 +101,66 @@
             },
             focus(){
             },
+            addItem(store, data) {
+                if (data.Item == null)
+                    data.Item = [];
+                data.Item.push({Value: '新路由项'});
+            },
+            addRouter(scope){
+                console.log(scope);
+                if (scope.row.Set == null)
+                    scope.row.Set = [];
+                scope.row.Set.push({"Name": "新路由", Item: []});
+            },
+            removeGroup(scope){
+                console.log(scope);
+                let arr = scope.store.table.data;
+                arr.splice($.inArray(scope.row, arr), 1);
+            },
+            removeRouter(node, store, data) {
+                console.log('remove', node, store, data);
+                let arr = node.parent.data;
+                arr.splice($.inArray(data, arr), 1);
+            },
+            removeItem(node, store, data) {
+                console.log('remove', node, store, data);
+                let arr = node.parent.data.Item;
+                arr.splice($.inArray(data, arr), 1);
+            },
+            textchanged(e, data, key) {
+                if (key)
+                    data[key] = e.target.value;
+                else
+                    data = e.target.value;
+            },
+            renderSourceRouter(h, {node, data, store}) {
+                console.log(node, data, store);
+                if (node.parent.parent) {
+                    return (<span>
+                            <input class="cellEditor" value={data['Value'] }
+                                   on-change={(e) => this.textchanged(e, data, 'Value')}/>
+
+                            <span style="float: right; margin-right: 20px;margin-top:5px">
+                              <el-button size="mini"
+                                         on-click={ () => this.removeItem(node, store, data) }>&nbsp;
+                                  -&nbsp; </el-button>
+                            </span>
+                      </span>);
+                } else
+                    return (
+                            <span>
+                                <input class="cellEditor" value={data['Name'] }
+                                       on-change={(e) => this.textchanged(e, data, 'Name')}/>
+
+                            <span style="float: right;position:absolute;right:0px; margin-right: 20px;margin-top:5px">
+                              <el-button size="mini" on-click={ () => this.addItem(store, data) }>&nbsp;
+                                  +&nbsp;</el-button>
+                              <el-button size="mini"
+                                         on-click={ () => this.removeRouter(node, store, data) }>&nbsp;
+                                  -&nbsp;</el-button>
+                            </span>
+                      </span>);
+            }
         },
         components: {
             editorContainer: editorContainer,
