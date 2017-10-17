9c708ab5bc1bb99f1a52766d1501de1014667151
liling <1045479083@qq.com>
2017-10-17 16:26:47 +0800

    +增加加载技术组件业务组件接口

--- server/service/afa.service.js
+++ server/service/afa.service.js
@@ -1,45 +1,54 @@
 /**
  * Created by zcn on 2017/6/15.
  */
-var ioService = require('./IOService');
-module.exports =  {
-    type:'afa',
-    services:[
-        {
-            id:'getNaviItems',
-            type:'IOService',
-            handler:ioService
-        },
-        {
-            id:'getNaviMenu',
-            type:'IOService',
-            handler:ioService
-        },
-        {
-            id:'getFile',
-            type:'IOService',
-            handler:ioService
-        },
-        {
-            id:'createNewResource',
-            type:'IOService',
-            handler:ioService
-        },
-        {
-            id:'local1',
-            type:'localService',
-            handler:function () {
-                console.info('run afa localService: local1');
-            }
-        },
-        {
-            id:'afaCompile',
-            type:'IOService',
-            handler:ioService
-        },{
-            id:'saveFile',
-            type:'IOService',
-            handler:ioService
-        }
-    ]
+var ioService = require('./IOService')
+module.exports = {
+  type: 'afa',
+  services: [
+    {
+      id: 'getNaviItems',
+      type: 'IOService',
+      handler: ioService
+    },
+    {
+      id: 'getNaviMenu',
+      type: 'IOService',
+      handler: ioService
+    },
+    {
+      id: 'getFile',
+      type: 'IOService',
+      handler: ioService
+    },
+    {
+      id: 'createNewResource',
+      type: 'IOService',
+      handler: ioService
+    },
+    {
+      id: 'local1',
+      type: 'localService',
+      handler: function () {
+        console.info('run afa localService: local1')
+      }
+    },
+    {
+      id: 'afaCompile',
+      type: 'IOService',
+      handler: ioService
+    }, {
+      id: 'saveFile',
+      type: 'IOService',
+      handler: ioService
+    }, {
+      id: 'loadBcpt',
+      type: 'IOService',
+      handler: ioService
+    }
+    , {
+      id: 'loadTcpt',
+      type: 'IOService',
+      handler: ioService
+    }
+  ]
 }
