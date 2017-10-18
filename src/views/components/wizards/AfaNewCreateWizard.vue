38a51cfc474a4197156229029011811372903527
liling <1045479083@qq.com>
2017-10-18 11:30:29 +0800

    *新建信息验证

--- src/views/components/wizards/AfaNewCreateWizard.vue
+++ src/views/components/wizards/AfaNewCreateWizard.vue
@@ -74,32 +74,43 @@
 
       },
       handleOk(){
-        this.dialogFormVisible = false;
-        IDE.socket.emit("createNewResource",{
-          type: IDE.type,
-          event: 'createNewResource',
-          data: {path: this.path,resourceId:this.resourceId,type:this.type,name:this.name,description:this.desc}
-        }, function (result) {
-          if (result) {
-            if (result.state === 'success') {
-              let path = result.data.path;
-              //刷新
-              if(path)
-                IDE.navigator.refresh(path);
-              else
-                IDE.navigator.refresh(null);
-              let type = result.data.type;
-              if (type === 'file') {
-                let item = IDE.navigator.getItem(path);
-                let input = result.data.input;
-                if(input){
+
+        if(!this.name||!this.desc){
+           alert("名字和描述不能为空");
+        }else {
+          this.dialogFormVisible = false;
+          IDE.socket.emit("createNewResource", {
+            type: IDE.type,
+            event: 'createNewResource',
+            data: {
+              path: this.path,
+              resourceId: this.resourceId,
+              type: this.type,
+              name: this.name,
+              description: this.desc
+            }
+          }, function (result) {
+            if (result) {
+              if (result.state === 'success') {
+                let path = result.data.path;
+                //刷新
+                if (path)
+                  IDE.navigator.refresh(path);
+                else
+                  IDE.navigator.refresh(null);
+                let type = result.data.type;
+                if (type === 'file') {
+                  let item = IDE.navigator.getItem(path);
+                  let input = result.data.input;
+                  if (input) {
                     //打开编辑器
-                  IDE.editorPart.openEditor(item,input);
+                    IDE.editorPart.openEditor(item, input);
+                  }
                 }
               }
             }
-          }
-        });
+          });
+        }
       },
       handleBroswer(){
         //获取相关引用
