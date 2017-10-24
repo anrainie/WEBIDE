2955e81f27e5de6611675a16714741f298550151
XyParaCrim <1105120410@qq.com>
2017-10-24 17:15:32 +0800

    修正bug

--- src/views/components/editor/flowEditor/fcEditor/propDialog.js
+++ src/views/components/editor/flowEditor/fcEditor/propDialog.js
@@ -7,7 +7,23 @@ import debugGroup from '../../../flowPropDialog/debugGroup.vue'
 let baseDialogComponent = {
     template: `
                 <el-dialog :modal-append-to-body="false" title="组件属性" :visible="showProperties" @update:visible="updateVisible" size="small">
-                 <el-collapse v-model="activeName" value="1" v-if="showProperties">
+                
+                <div v-if="showProperties">
+                <el-tabs v-if="group instanceof Array" type="card">
+                    <el-tab-pane v-for="tab in group" :label="tab.name">
+                        <el-collapse v-model="activeName">
+                        
+                            <el-collapse-item  :title="item.name" :name="index" v-for="(item, name, index) in tab.config">
+                                <keep-alive>
+                                    <component :is="name" :model="getProps" :type="item.type" :ref="name" :path="path"></component>
+                                </keep-alive>
+                            </el-collapse-item>
+                        
+                        </el-collapse>
+                    </el-tab-pane>
+                </el-tabs>
+                
+                 <el-collapse v-else v-model="activeName" >
                         
                         <el-collapse-item  :title="item.name" :name="index" v-for="(item, name, index) in group">
                             <keep-alive>
@@ -15,7 +31,8 @@ let baseDialogComponent = {
                             </keep-alive>
                         </el-collapse-item>
                         
-                    </el-collapse>
+                 </el-collapse>
+                 </div>
                         
                     <span slot="footer" class="dialog-footer" v-if="showProperties">
                         <el-button @click="updateVisible(false)">取消</el-button>
@@ -56,8 +73,16 @@ let createDialog = function (config) {
     let result = {};
     if (config) {
         result.components = {};
-        for (let [refName, com] of Object.entries(config)) {
-            result.components[refName] = com["group"]
+        if (config instanceof  Array) {
+            config.forEach((child) => {
+                for (let [refName, com] of Object.entries(child.config)) {
+                    result.components[refName] = com["group"]
+                }
+            })
+        } else {
+            for (let [refName, com] of Object.entries(config)) {
+                result.components[refName] = com["group"]
+            }
         }
     }
 
@@ -104,17 +129,30 @@ export let stepDialogs = {
         }
     }),
     //多出口组件
-    step7: createDialog({
-        basicInfo: {
-            name: "基本信息",
-            group: basicInfo,
-            type: '3'
-        },
-        skipInfo: {
-            name: '伪执行',
-            group: skipGroup
+    step7: createDialog([
+        {
+            name:"step信息",
+            config: {
+                basicInfo: {
+                    name: "基本信息",
+                    group: basicInfo,
+                    type: '3'
+                },
+                skipInfo: {
+                    name: '伪执行',
+                    group: skipGroup
+                }
+            }},
+        {
+            name: "step出口",
+            config: {
+                skipInfo: {
+                name: '伪执行',
+                group: skipGroup
+                }
+            }
         }
-    }),
+    ]),
     //bcpt
     step4: createDialog({
         basicInfo: {
