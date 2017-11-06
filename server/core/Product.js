62ebc9e1f6c7b4c4a9ca171e5960cf203351693b
zcn <912555919@qq.com>
2017-11-06 14:20:56 +0800

    修改Servlet、server的bugs

--- server/core/Product.js
+++ server/core/Product.js
@@ -5,98 +5,102 @@ var Client = require('socket.io-client');
 var dbConstants = require('../constants/DBConstants');
 var productDao = require('../dao/ProductDao');
 
-function Product(socket,id,type,ip) {
-    this.id = id;
-    this.type = type;
-    this.ip = ip;
-    this.socket = socket;
-    this.clients = {};
-}
+class Product{
+    
+    constructor(socket,id,type,ip){
+        this.id = id;
+        this.type = type;
+        this.ip = ip;
+        this.socket = socket;
+        this.clients = {};
+    }
+   
 
-Product.prototype.runServiceHandler = function (reqData, callback) {
-    if(!this.socket.connected) {
-        callback({state: "error", errorMsg:"Product is disconnected"});
-    }else{
-        IDE.consoleLogger.debug(`product emit ${reqData.event}`);
-        this.emit(reqData.event,JSON.stringify(data),(respData) => {
-            IDE.consoleLogger.debug(`product emit callback successful: ${reqData.event}`);
+    runServiceHandler  (reqData, callback) {
+        if(!this.socket.connected) {
+            callback({state: "error", errorMsg:"Product is disconnected"});
+        }else{
+            IDE.consoleLogger.debug(`product emit ${reqData.event}`);
+            this.emit(reqData.event,JSON.stringify(data),(respData) => {
+                IDE.consoleLogger.debug(`product emit callback successful: ${reqData.event}`);
+                callback(respData);
+            });
+        }
+    }
+
+    emit  (eventId,reqData,callback) {
+        this.socket.emit(eventId, reqData, function (respData) {
             callback(respData);
         });
     }
-}
 
-Product.prototype.emit = function (eventId,reqData,callback) {
-    this.socket.emit(eventId, reqData, function (respData) {
-        callback(respData);
-    });
-}
+    /**
+     *
+     * @param reqData {uid:'',path:''}
+     * @param callback
+     */
+    lockFile  (reqData,callback) {
+        let self = this;
+        let data = reqData.data;
+        let cb = callback;
+        this.emit("lockFile", JSON.stringify(reqData), function (respData) {
+            cb(respData);
+        });
+    }
 
-/**
- *
- * @param reqData {uid:'',path:''}
- * @param callback
- */
-Product.prototype.lockFile = function (reqData,callback) {
-    let self = this;
-    let data = reqData.data;
-    let cb = callback;
-    this.emit("lockFile", JSON.stringify(reqData), function (respData) {
-        cb(respData);
-    });
-}
+    /**
+     *
+     * @param reqData {uid:'',path:''}
+     * @param callback
+     */
+    releaseFile  (reqData,callback) {
+        let self = this;
+        let cb = callback;
+        let data = reqData.data;
+        this.emit('releaseFilelock', JSON.stringify(reqData), function (result) {
+            cb(result);
+        });
+    }
 
-/**
- *
- * @param reqData {uid:'',path:''}
- * @param callback
- */
-Product.prototype.releaseFile = function (reqData,callback) {
-    let self = this;
-    let cb = callback;
-    let data = reqData.data;
-    this.emit('releaseFilelock', JSON.stringify(reqData), function (result) {
-        cb(result);
-    });
-}
+    /**
+     *
+     * @param reqData {path:''}
+     * @param callback
+     */
+    peekFileLock  (reqData,callback) {
+        let cb = callback;
+        this.emit('peekFileLock', JSON.stringify(reqData), function (result) {
+            cb(result);
+        });
+    }
 
-/**
- *
- * @param reqData {path:''}
- * @param callback
- */
-Product.prototype.peekFileLock = function (reqData,callback) {
-    let cb = callback;
-    this.emit('peekFileLock', JSON.stringify(reqData), function (result) {
-        cb(result);
-    });
-}
+    getClientNum  () {
+        return Object.getOwnPropertyNames(this.clients).length;
+    }
 
-Product.prototype.getClientNum = function () {
-    return Object.getOwnPropertyNames(this.clients).length;
-}
+    addClient  (id,client) {
+        this.clients[id] = client;
+    }
 
-Product.prototype.addClient = function (id,client) {
-    this.clients[id] = client;
-}
+    removeClient  (id) {
+        delete this.clients[id];
+    }
 
-Product.prototype.removeClient = function (id) {
-    delete this.clients[id];
-}
+    getClient  (uid) {
+        return this.clients[uid];
+    }
 
-Product.prototype.getClient = function (uid) {
-    return this.clients[uid];
-}
+    disconnect  () {
+        this.socket.close();
+    }
 
-Product.prototype.disconnect = function () {
-    this.socket.close();
-}
+    unregister  (p) {
+        productDao.delProduct({'id':this.id});
+        let p_u = IDE.DB.getCollection(dbConstants.PRODUCT_USER);
+        p_u.findAndRemove({id:this.id});
+        IDE.ideLogger.info(`unregister product ${this.ip} -${this.type}`)
+    }
 
-Product.prototype.unregister = function (p) {
-    productDao.delProduct({'id':this.id});
-    let p_u = IDE.DB.getCollection(dbConstants.PRODUCT_USER);
-    p_u.findAndRemove({id:this.id});
-    IDE.ideLogger.info(`unregister product ${this.ip} -${this.type}`)
 }
-
 module.exports =  Product;
 
--- server/core/Servlet.js
+++ server/core/Servlet.js
@@ -11,184 +11,190 @@ const Product = require('./Product');
 const ideServices = require('./../service/ide.service.js');
 
 
-function Servlet(serviceConfigs, session, http) {
-    this.serviceConfigs = serviceConfigs;
-    this.session = session;
-    this.http = http;
-    this.products = [];
-    this.user2product = new Map();
-}
+class Servlet{
 
-Servlet.prototype.start = function () {
-    var self = this;
-    var server = socket_io(this.http);
-
-    for(let i = 0 ; i < this.serviceConfigs.length; i++){
-        IDE.mergeService(ideServices,this.serviceConfigs[i]);
+    constructor(serviceConfigs, session, http){
+        this.serviceConfigs = serviceConfigs;
+        this.session = session;
+        this.http = http;
+        this.products = [];
+        this.user2product = new Map();
     }
 
-    server.use(shareSession(this.session, {
-        autoSave: true
-    }));
+    start () {
+        var self = this;
+        var server = socket_io(this.http);
 
-    server.use(function (socket, next) {
-        var user = socket.handshake.session.user;
-        let isServer = socket.handshake.query.server;
-        if (isServer || user) {
-            next();
-        } else {
-            next(new Error('nosession'));
+        for(let i = 0 ; i < this.serviceConfigs.length; i++){
+            IDE.mergeService(ideServices,this.serviceConfigs[i]);
         }
-    });
 
-    server.on('connection', function (socket) {
-        let user = socket.handshake.session.user;
-        let idetype = socket.handshake.query.type;
-        let isServer = socket.handshake.query.server;
-        if(isServer) {
-            let id = socket.handshake.query.id;
-            let ip = socket.handshake.address;
-            let product = new Product(socket,id,idetype,ip);
-            self.addProduct(product);
-        }else{
-            self.addClient(idetype, user, socket);
-        }
-    });
-}
+        server.use(shareSession(this.session, {
+            autoSave: true
+        }));
 
-Servlet.prototype.addProduct = function (product) {
-    this.products.push(product);
-    IDE.ideLogger.info(`product is connected : ${product.ip} - ${product.type}`);
-
-    product.socket.on('disconnect',() => {
-        this.products.every((value,index) => {
-            if(value === product){
-                this.products.splice(index,1);
-                return false;
+        server.use(function (socket, next) {
+            var user = socket.handshake.session.user;
+            let isServer = socket.handshake.query.server;
+            if (isServer || user) {
+                next();
+            } else {
+                next(new Error('nosession'));
             }
-            return true;
         });
 
-        let clients = product.clients;
-        var uids = Object.getOwnPropertyNames(clients);
-        uids.forEach( (v,k) => {
-            this.user2product.delete(k);
-        });
-
-        product.disconnect();
-    });
-
-}
-
-Servlet.prototype.addClient  = function (idetype,user,socket) {
-    let uid = user["id"];
-    IDE.defaultLogger.info(uid + ' connect socket successful');
-
-    let product = this.assignProduct(idetype,user);
-    if(product){
-        product.addClient(uid,socket);
-        this.user2product.push(uid,product);
-    }
-
-    socket.on('disconnect', function () {
-        if(product) {
-            product.removeClient(uid);
-            this.user2product.delete(uid);
-        }
-    });
-
-    let services = this.getService(idetype);
-    services.forEach((service,index) => {
-        if (service.id && service.type) {
-            if (service.type === 'IOService') {
-                socket.on(idetype + "_" + service.id, (reqData, callback) => {
-                    reqData.uid = uid;
-                    let p = this.user2product.get(uid);
-                    if (p) {
-                        p.runServiceHandler(reqData, callback);
-                    } else {
-                        callback({state:'error',errorMsg:'can not find product :' + reqData.type, reqData});
-                    }
-                });
-            } else if (service.type === 'localService') {
-                socket.on(idetype + "_" + service.id, (reqData, callback) => {
-                    reqData.uid = uid;
-                    let p = this.user2product.get(uid);
-                    service.handler.call(this, reqData, callback, p, service);
-                });
+        server.on('connection', function (socket) {
+            let user = socket.handshake.session.user;
+            let idetype = socket.handshake.query.type;
+            let isServer = socket.handshake.query.server;
+            if(isServer) {
+                let id = socket.handshake.query.id;
+                let ip = socket.handshake.address;
+                let product = new Product(socket,id,idetype,ip);
+                self.addProduct(product);
+            }else{
+                self.addClient(idetype, user, socket);
             }
-        }
-    });
-}
-
-Servlet.prototype.assignProduct = function (idetype,user) {
-    let uid = user['_id'];
-    let p_u = IDE.DB.getCollection("product_user");
-    let db_pu = p_u.find({uid});
-    if(db_pu && db_pu.length > 0){
-        for(let i = 0 ; i < db_pu.length ; i ++){
-            let p = this.getProduct(db_pu[i].pid);
-            if(p && p.type === idetype){
-                return p;
-            }
-        }
-    }
-
-    let min ;
-    let selected;
-    let products = this.getAllProducts();
-    for(let i = 0 ; i < products.length ; i++) {
-        let p = products[i];
-        let pNum = p_u.count({'pid':p.id});
-        if(idetype === p.type && (!selected || pNum < min)){
-            min = pNum;
-            selected = p;
-        }
-    }
-    if(selected) {
-        p_u.insert({
-            'uid': uid,
-            'pid': selected.id,
-            'createTime': new Date()
         });
     }
-    return selected;
-}
 
-Servlet.prototype.getProduct = function (id) {
-    for(let i = 0 ; i < this.products.length ; i++){
-        if(this.products[i].id === id){
-            return this.products[i];
+    addProduct(product) {
+        this.products.push(product);
+
+        IDE.ideLogger.info(`Product ${product.ip} - ${product.type} is connected`);
+
+        product.socket.on('disconnect',() => {
+            this.products.every((value,index) => {
+                if(value === product){
+                    this.products.splice(index,1);
+                    return false;
+                }
+                return true;
+            });
+
+            let clients = product.clients;
+            var uids = Object.getOwnPropertyNames(clients);
+            uids.forEach( (v,k) => {
+                this.user2product.delete(k);
+            });
+
+            product.disconnect();
+
+            IDE.ideLogger.info(`Product ${product.ip} - ${product.type} is disconnected`);
+        });
+
+    }
+
+    addClient (idetype,user,socket) {
+        let uid = user["id"];
+        IDE.defaultLogger.info(uid + ' connect socket successful');
+
+        let product = this.assignProduct(idetype,user);
+        if(product){
+            product.addClient(uid,socket);
+            this.user2product.push(uid,product);
+        }
+
+        socket.on('disconnect', function () {
+            if(product) {
+                product.removeClient(uid);
+                this.user2product.delete(uid);
+            }
+        });
+
+        let services = this.getService(idetype);
+        services.forEach((service,index) => {
+            if (service.id && service.type) {
+                if (service.type === 'IOService') {
+                    socket.on(idetype + "_" + service.id, (reqData, callback) => {
+                        reqData.uid = uid;
+                        let p = this.user2product.get(uid);
+                        if (p) {
+                            p.runServiceHandler(reqData, callback);
+                        } else {
+                            callback({state:'error',errorMsg:'can not find product :' + reqData.type, reqData});
+                        }
+                    });
+                } else if (service.type === 'localService') {
+                    socket.on(idetype + "_" + service.id, (reqData, callback) => {
+                        reqData.uid = uid;
+                        let p = this.user2product.get(uid);
+                        service.handler.call(this, reqData, callback, p, service);
+                    });
+                }
+            }
+        });
+    }
+
+    assignProduct (idetype,user) {
+        let uid = user['_id'];
+        let p_u = IDE.DB.getCollection("product_user");
+        let db_pu = p_u.find({uid});
+        if(db_pu && db_pu.length > 0){
+            for(let i = 0 ; i < db_pu.length ; i ++){
+                let p = this.getProduct(db_pu[i].pid);
+                if(p && p.type === idetype){
+                    return p;
+                }
+            }
+        }
+
+        let min ;
+        let selected;
+        let products = this.getAllProducts();
+        for(let i = 0 ; i < products.length ; i++) {
+            let p = products[i];
+            let pNum = p_u.count({'pid':p.id});
+            if(idetype === p.type && (!selected || pNum < min)){
+                min = pNum;
+                selected = p;
+            }
+        }
+        if(selected) {
+            p_u.insert({
+                'uid': uid,
+                'pid': selected.id,
+                'createTime': new Date()
+            });
+        }
+        return selected;
+    }
+
+    getProduct (id) {
+        for(let i = 0 ; i < this.products.length ; i++){
+            if(this.products[i].id === id){
+                return this.products[i];
+            }
+        }
+        return null;
+    }
+
+    getService (type) {
+        for(var j = 0 ; j < this.serviceConfigs.length ; j++){
+            if(this.serviceConfigs[j].type === type){
+                return this.serviceConfigs[j];
+            }
+        }
+        return [];
+    }
+
+    getAllProducts () {
+        return this.products;
+    }
+
+    removeProduct (id) {
+        for(let i = 0 ; i < this.products.length ; i++){
+            if(this.products[i].id === id){
+                this.products.splice(i ,1);
+                break;
+            }
         }
     }
-    return null;
-}
 
-Servlet.prototype.getService = function (type) {
-    for(var j = 0 ; j < this.serviceConfigs.length ; j++){
-        if(this.serviceConfigs[j].type === type){
-            return this.serviceConfigs[j];
-        }
+    unregisterProduct (p) {
+        p.disconnect();
+        p.unregister();
     }
-    return null;
-}
-
-Servlet.prototype.getAllProducts = function () {
-    return this.products;
-}
-
-Servlet.prototype.removeProduct = function (id) {
-    for(let i = 0 ; i < this.products.length ; i++){
-        if(this.products[i].id === id){
-            this.products.splice(i ,1);
-            break;
-        }
-    }
-}
-
-Servlet.prototype.unregisterProduct = function (p) {
-    p.disconnect();
-    p.unregister();
 }
 
 module.exports = Servlet
